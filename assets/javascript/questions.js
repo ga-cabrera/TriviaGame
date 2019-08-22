8f2e92e81c6de0c7b5ceb61c39ed8e685042299a
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-08-21 18:02:10 -0700

    added [fake] questions to questions.js

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,1 +1,52 @@
-console.log($);
+var batmanQuestions = [
+    {
+        question: "What is Batman 1?",
+        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
+        answer: "Answer TWO"
+    },
+    {
+        question: "What is Batman 2?",
+        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
+        answer: "Answer TWO"
+    },
+    {
+        question: "What is Batman 3?",
+        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
+        answer: "Answer TWO"
+    },
+    {
+        question: "What is Batman 4?",
+        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
+        answer: "Answer TWO"
+    },
+    {
+        question: "What is Batman 5?",
+        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
+        answer: "Answer TWO"
+    },
+    {
+        question: "What is Batman 6?",
+        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
+        answer: "Answer TWO"
+    },
+    {
+        question: "What is Batman 7?",
+        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
+        answer: "Answer TWO"
+    },
+    {
+        question: "What is Batman 8?",
+        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
+        answer: "Answer TWO"
+    },
+    {
+        question: "What is Batman 9?",
+        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
+        answer: "Answer TWO"
+    },
+    {
+        question: "What is Batman 10?",
+        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
+        answer: "Answer TWO"
+    }
+]
