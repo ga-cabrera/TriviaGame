f96fb0f6c5cb94fd1b92ef26397d0170cd07399f
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-08-21 18:17:21 -0700

    added loadQuestions function that let's us how our questions from questions.js on the webpage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,15 @@
-console.log($);
+// Global Values
+let counter=30;
+let currentQuestion=0;
+let core=0;
+let lost=0;
+let timer;
+
+// Display questions and choices together
+function loadQuestion() {
+    var question = batmanQuestions[currentQuestion].question;
+    var choices = batmanQuestions[currentQuestion].choices;
+    $("#timer").html('Time Left: ' + counter);
+    $("#game").html(`<h4> ${question} </h4>`);
+    };
+loadQuestion();
