732eb89bfb178a07e041b4a5ed160d2133fa19d9
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-08-23 21:11:34 -0700

    added border to choice boxes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 // Global Values
 let counter=30;
-let currentQuestion=0;
+let currentQuestion=1;
 let core=0;
 let lost=0;
 let timer;
@@ -10,6 +10,20 @@ function loadQuestion() {
     var question = batmanQuestions[currentQuestion].question;
     var choices = batmanQuestions[currentQuestion].choices;
     $("#timer").html('Time Left: ' + counter);
-    $("#game").html(`<h4> ${question} </h4>`);
-    };
+    $("#game").html(`
+    <h4> ${question} </h4>
+    ${loadChoices(choices)}
+    `);
+};
+
+
+// Dispay all our choices at once
+function loadChoices(choices) {
+    let result='';
+
+    for(let i=0;i<choices.length; i++) {
+        result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`;
+    }
+    return result;
+}
 loadQuestion();
