fbf15b4d0f7bce3b3b701c43cc3e69b38cab6328
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-08-25 02:42:29 -0700

    added comments to keep logic organized, added an intro monologue functions for fun

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,30 @@
-// Global Values
-
+//  --------------- GLOBAL VARIABLES ------------------ //
 let counter=15;
 let currentQuestion=0;
 let score=0;
+let timer;
+var document = new Document();
 // answerCounter & answerTimer are for transitions between selected answer and next question
 let answerCounter = 5;
 let answerTimer;
-// --------------------------
-let timer;
-var document = new Document();
 
+
+// ----------------------- FUNCTIONS ----------------------- //
+
+// Intro Monologue from The Riddler, will disappear once game begins 
+function displayIntro() {
+    var welcome = `
+    <h1>Riddler Me This!</h1>
+    <p>What's nowhere but everywhere, except where something <b>Was</b>?</p>
+    <h3>GOTHAM AFTER I BLOW IT TO SMITHEREENS!</h3>
+    <p>I hope your favorite triva topic is <b>YOU</b>, Batman! because the fate of your precious city depends on it!</p>
+    <p>Let Us Begin!</p>
+    `
+    $("#game").html(welcome);
+}
+displayIntro();
+
+// function that loads up next object (question) in batmanQuestion Array in questions.js. will also call endGame function when there are no more questions
 function nextQuestion() {
     clearInterval(answerTimer);
 
@@ -24,6 +39,7 @@ function nextQuestion() {
     };
 }
 
+// When there are no more questions left in batmanQuestion Array, this function is called to see if you win or lose, and resets game if you so choose, with reset button.
 function endGame () {
     var gameOver = `
     <h1>Game Over</h1>
@@ -37,7 +53,7 @@ function endGame () {
     <p> Guess that's enough to keep your precious Gotham alive. Until next time, Batman!</p>
     <button class= "btn btn-dark" id= "reset">Play Again</button>
     `
-    if (score < 4) {
+    if (score < 6) {
         $("#game").html(gameOver);
     }
 
@@ -46,7 +62,7 @@ function endGame () {
     }
 }
 
-// click function for resetting game
+// grabs id="reset" (the reset button in endGame function) and when clicked, will reset game from the top
 $(document).on('click', '#reset', function(){
     counter = 15;
     currentQuestion = 0;
@@ -55,6 +71,7 @@ $(document).on('click', '#reset', function(){
     loadQuestion();
 })
 
+// function that counts down our AnswerCounter from 5 to 0. whenever answerCounter===0, next questions is loaded on screen.
 function transitionCountDown () {
     if (answerCounter > 0) {
         answerCounter--;
@@ -66,6 +83,7 @@ function transitionCountDown () {
     console.log(answerCounter)
 }
 
+// Whenver var counter === 0, time is up and the following is presented to the screen, which then starts the transitionTimer, counting down the answerCounter from 5 to 0.
 function timeUp() {
 
     var outOfTime = `
@@ -77,6 +95,7 @@ function timeUp() {
     answerTimer = setInterval(transitionCountDown, 1000);
 }
 
+// function that lets counter count down from 15 to 0. whenever counter === 0, the timeUp function is called.
 function countDown() {
     if (counter > 0) {
         counter--;
@@ -88,7 +107,12 @@ function countDown() {
     $('#time').html('Time Left: ' + counter);
 }
 
-// Display questions and choices together
+// Whenever we click on the element with the id of startButton (begin button), the loadQuestion function is called.
+$("#startButton").click(function() {
+    loadQuestion();
+})
+
+// Function that displays question on the screen via id = "game" and displays our timer via id ="time".
 function loadQuestion() {
     $("#startButton").hide();
     counter=15;
@@ -105,7 +129,7 @@ function loadQuestion() {
 };
 
 
-// Dispay all our choices at once
+// function that loads all our choices from our batmanQuestion Array
 function loadChoices(choices) {
     let result='';
 
@@ -116,21 +140,20 @@ function loadChoices(choices) {
 }
 
 
-
+// function that compares the answer we chose verses the correct answer. If answer is correct, var right will be displayed and var score will increment. if answer is incorrect, var wrong will be diplayed.
 function questionAnswered(selAnswer, corAnswer) {
     var right= `
     <h1>Correct!</h1>
-    <h3>Correct Answer: ${batmanQuestions[currentQuestion].answer}</h3>
+    <h3>Answer: ${corAnswer}</h3>
     <p>Keep 'em coming, Batman! Gotham is DYING to see you win.</p>
     `
 
     var wrong = `
     <h1>Wrong!</h1>
-    <h3>Correct Answer: ${batmanQuestions[currentQuestion].answer}</h3>
+    <h3>Your Pathetic Answer: ${selAnswer}
+    <h3>Correct Answer: ${corAnswer}</h3>
     <p>Bats! Your memory fails you! Enough of these and Gotham will be a pile of rubble!</p>
     `
-    
-    
     if (selAnswer===corAnswer) {
         $("#game").html(right);
         console.log("You are correct!")
@@ -147,11 +170,6 @@ function questionAnswered(selAnswer, corAnswer) {
     console.log("MYYY ANSWERR: " + corAnswer);
 }
 
-
-$("#startButton").click(function() {
-loadQuestion();
-
-})
 // choice Buttons
 $(document).on('click', '.choice', function(){
     var selectedAnswer = $(this).attr('data-answer');
