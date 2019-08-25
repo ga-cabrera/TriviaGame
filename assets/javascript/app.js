90c82075fc399d65bc781b1c4bf55b47ffcaa03f
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-08-24 18:52:11 -0700

    created function that holds if, correct or incorrect answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
 
 let counter=15;
 let currentQuestion=0;
-let core=0;
+let score=0;
 let lost=0;
 let timer;
 var document = new Document();
@@ -27,12 +27,14 @@ function timeUp() {
 }
 
 function countDown() {
-    counter--;
-    $('#time').html('Time Left: ' + counter);
+    if (counter > 0) {
+        counter--;
+    }
 
     if (counter===0) {
         timeUp();
     }
+    $('#time').html('Time Left: ' + counter);
 }
 
 // Display questions and choices together
@@ -62,14 +64,32 @@ function loadChoices(choices) {
 }
 
 
+
+function questionAnswered(selAnswer, corAnswer) {
+    if (selAnswer===corAnswer) {
+        alert("congrats! That is correct");
+        console.log("You are correct!")
+        score++;
+        
+    }
+    else {
+        alert("You lose, Batsy. Enough of these and Gotham will be a pile of rubble!!")
+
+    }
+    clearInterval(timer);
+    nextQuestion();
+    console.log("your peasant answer: " + selAnswer);
+    console.log("MYYY ANSWERR: " + corAnswer);
+}
+
+
 $("#startButton").click(function() {
 loadQuestion();
 
 })
-
+// choice Buttons
 $(document).on('click', '.choice', function(){
     var selectedAnswer = $(this).attr('data-answer');
     var correctAnswer = batmanQuestions[currentQuestion].answer;
-    console.log("your peasant answer: " + selectedAnswer);
-    console.log("MYYY ANSWERR: " + correctAnswer);
+    questionAnswered(selectedAnswer, correctAnswer);
 })
