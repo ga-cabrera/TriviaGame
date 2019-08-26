04744ff32fe9c13afb42b419075fb6f964d51169
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-08-26 07:35:39 -0700

    replaced filler questions and answers with realy questions and answers

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,52 +1,52 @@
 var batmanQuestions = [
     {
-        question: "What is Batman 1?",
-        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
-        answer: "Answer TWO"
+        question: "Who played Batman in Batman v Superman: Dawn of Justice (2016)?",
+        choices: ["George Clooney", "Christian Bale", "Ben Affleck", "", "Robert Pattinson"],
+        answer: "Ben Affleck"
     },
     {
-        question: "What is Batman 2?",
-        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
-        answer: "Answer TWO"
+        question: "What villain did Arnold Schwarzenegger play in Batman & Robin?",
+        choices: ["Poison Ivy", "Two-Face", "Bane", "Mr. Freeze", "Joker"],
+        answer: "Mr. Freeze"
     },
     {
-        question: "What is Batman 3?",
-        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
-        answer: "Answer TWO"
+        question: "What superpower does the Joker have?",
+        choices: ["Talk to Fish", "Poison Breath", "Super Strength", "None", "Super Intelligent"],
+        answer: "None"
     },
     {
-        question: "What is Batman 4?",
-        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
-        answer: "Answer TWO"
+        question: "Which of these Bat-villains was introduced first?",
+        choices: ["The Penguin", "Catwoman", "Two-Face", "The Ridder (Yours Truly)", "Mr. Freeze"],
+        answer: "Catwoman"
     },
     {
-        question: "What is Batman 5?",
-        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
-        answer: "Answer TWO"
+        question: "What former District Attorney became the villain known as Two-Face?",
+        choices: ["Jason Blood", "Edward Nygma", "Lex Luthor", "Bruce Wayne", "Harvey Dent"],
+        answer: "Harvey Dent"
     },
     {
-        question: "What is Batman 6?",
-        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
-        answer: "Answer TWO"
+        question: "What super villain once broke Batman's back, leaving him crippled and wheelchair-bound?",
+        choices: ["Killer Croc", "Joker", "Bane", "Ra's Al Ghul", "Hush"],
+        answer: "Bane"
     },
     {
-        question: "What is Batman 7?",
-        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
-        answer: "Answer TWO"
+        question: "Who first replaced Batman when he was paralyzed by Bane?",
+        choices: ["Red Robin", "Nightwing", "Azrael", "Christain Bale", "Red Hood"],
+        answer: "Azrael"
     },
     {
-        question: "What is Batman 8?",
-        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
-        answer: "Answer TWO"
+        question: "What writer penned the four-issue 1986 comic book miniseries 'The Dark Knight Returns'?",
+        choices: ["Frank Miller", "Bob Kane", "Scott Snyder", "Stan Lee", "Grant Morrison"],
+        answer: "Frank Miller"
     },
     {
-        question: "What is Batman 9?",
-        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
-        answer: "Answer TWO"
+        question: "Which of the following characters did Bruce Wayne have a son with?",
+        choices: ["Poison Ivy", "Harley Quinn", "Catwoman", "Talia Al Ghul", "Barbara Gordan"],
+        answer: "Talia Al Ghul"
     },
     {
-        question: "What is Batman 10?",
-        choices: ["Answer ONE", "Answer TWO", "Answer THREE", "ETC", "ETC"],
-        answer: "Answer TWO"
+        question: "What writer first referred to Batman as the 'Dark Knight'?",
+        choices: ["Christopher Nolan", "Frank Miller", "Bill Finger", "Jack Kirby", "Scott Snyder"],
+        answer: "Bill Finger"
     }
 ]
